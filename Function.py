b921875353c406b393d758c22d40d02c56af7f32
Mohammad-Kaif45 <kaifmumtajansari@gmail.com>
2026-05-10 12:06:39 +0530

    Function code

---
+++ Function.py
@@ -0,0 +1,15 @@
+def hello():
+    print("Hello")
+
+hello()
+
+# Sum of two variable 
+def Sum(a,b): # here a and b is parameter
+    print(f"Sum of {a} and {b} is {a + b}")
+
+Sum(2,6) # here 2 and 6 is called arguments
+
+# there are three types of arguments
+# positional argument
+# keywords argument
+# default arguments
