3c6141f45511d935cb4a68c4dc79926c13fe1ccf
Mohammad-Kaif45 <kaifmumtajansari@gmail.com>
2026-05-10 17:26:50 +0530

    List

---
+++ List.py
@@ -0,0 +1,15 @@
+# List is mutable,allow duplicates element, orderd in nature, hetrogeneous 
+
+numbers = [1,2,3,4]
+
+numbers.append(10) # add 10 to the end
+
+numbers.insert(2,15) # insert 15 at index 2
+
+numbers.extend([16,19,20]) # add multiple elements as list
+
+numbers.remove(10) # remove the first occurance of 10
+
+
+print(numbers)
+
