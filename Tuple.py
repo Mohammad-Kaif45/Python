232a9a28796773129580aa48e828fc8fc687167f
Mohammad-Kaif45 <kaifmumtajansari@gmail.com>
2026-05-11 10:26:22 +0530

    Tuple

---
+++ Tuple.py
@@ -0,0 +1,19 @@
+# Tuple is immutable means you can not changes
+# You can have duplicates value in tuple
+# Tuple is orderd in nature
+# Tuple is also hetrogenous in nature
+
+# Tuple Traversing and methods
+
+# Tuples are traversed in the same manner as List are traversed
+# But remember tuples are like strings you can’t change anything once it’s made we can’t change them.
+# Well the use case is not much in question solving but still you have to understand it
+
+
+t = (5,2,1,2,7)
+print(t)
+
+index = t.index(2)
+count = t.count(2)
+
+print(index,count)
