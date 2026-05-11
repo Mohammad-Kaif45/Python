5ca2282eb04f50b2b56f76e1a61cbf7b2f421518
Mohammad-Kaif45 <kaifmumtajansari@gmail.com>
2026-05-11 12:08:35 +0530

    Set

--- Set.py
+++ Set.py
@@ -11,3 +11,33 @@
 # Only immutable (hashable) objects can be stored in a set
 # (e.g., numbers, strings, tuples). Mutable objects like lists and
 # dictionaries are not allowed
+
+s = {1,2,3,4}
+
+s.add(4) # add an elements to the set
+
+s.remove(2) # remove 2 (rises an error if not found)
+
+s.discard(7) # remove 7 (no error if not found)
+
+pop_element = s.pop() # remove a random element
+
+s.clear()
+print(s)
+
+# Now these are some of the basic methods but sets also have
+# some special methods for performing some special
+# operations between 2 sets.
+
+A = {1,2,3}
+B = {3,4,5}
+
+union = A.union(B)
+print(union)
+
+intersection = A.intersection(B)
+print(intersection)
+
+difference_set = A.difference(B)
+print(difference_set)
+
