51985a632690c06cc7d6980b8e884f8038334f30
Mohammad-Kaif45 <kaifmumtajansari@gmail.com>
2026-05-13 17:06:37 +0530

    Object Oriented Programming

---
+++ Advanced_Topic.py
@@ -0,0 +1,31 @@
+# Decorator : A decorator is just a function that modifies another function
+# without changing its actual code.
+
+
+def decorator(func):
+    def wrapper(*args, **kwargs):
+        print("Before the function is called")
+        result = func(*args, **kwargs)
+        print("After the function is called")
+        return result
+    return wrapper
+
+
+@decorator
+def hello(name):
+    print(f"Hello {name}")
+
+hello("World")
+
+# args and kwargs in decorator
+# They’re special keywords in Python used in function definitions to
+# accept a flexible number of arguments.
+# The important part is the * and ** syntax. You can use any name
+# in front of them, but *args and **kwargs are the common conventions.
+# *args is used for positional arguments and becomes a tuple.
+# **kwargs is used for keyword arguments and becomes a dictionary.
+
+
+
+
+
